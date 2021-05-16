0ad0257a3f50989bc36ac25cd745a9b701324011
yanis457824 <yanis457824@gmail.com>
2021-05-16 12:41:01 +0300

    Last Version

--- www/app/apps/docApprove/INV/invListCtrl.js
+++ www/app/apps/docApprove/INV/invListCtrl.js
@@ -53,8 +53,13 @@ angular.module('pele')
           var result = apiData.ROW || [];
           //Cursor if empty
           if (result.length && result[0].DOC_NAME === null) {
-            PelApi.appSettings.config.IS_TOKEN_VALID = 'N'
-            PelApi.goHome();
+           // PelApi.appSettings.config.IS_TOKEN_VALID = 'N'
+            //PelApi.goHome();
+            $state.go("app.p2_moduleList", {
+              "AppId":  $scope.appId,
+              "Title": "",
+              "Pin":  $state.pin
+            });
           }
 
 
